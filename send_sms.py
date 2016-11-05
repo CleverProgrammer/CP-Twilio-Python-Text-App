4c76dba41e8cda2669a1be0c196845e07d8012e6
Rafeh Qazi <rafehqazi1@gmail.com>
2016-11-04 22:07:59 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -4,7 +4,7 @@ from credentials import account_sid, auth_token, my_cell, my_twilio
 # Find these values at https://twilio.com/user/account
 client = TwilioRestClient(account_sid, auth_token)
 
-my_msg = ''.join(['silly bob\n' for i in range(1)])
+my_msg = "Your message goes here..."
 
 message = client.messages.create(to=my_cell, from_=my_twilio,
                                      body=my_msg)
