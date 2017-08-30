b63c39870fb1d3bcd1a148ac9a387f27dccf30ca
sweet74a <31387700+sweet74a@users.noreply.github.com>
2017-08-30 04:28:18 +0300

    renew
    the old is not work anymore

--- credentials.py
+++ credentials.py
@@ -2,3 +2,18 @@ account_sid = "Your Account Sid"
 auth_token = "Your Auth Token"
 my_cell = "+Your Cell Number"
 my_twilio = "+Your Twilio Number"
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "AC37b5699c2f2c158171b0d7b06881732b"
+# Your Auth Token from twilio.com/console
+auth_token  = "your_auth_token"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+15558675309", 
+    from_="+15017250604",
+    body="Hello from Python!")
+
+print(message.sid)
