58c1c1eb77c85a163a2f477304a8f0182fc69d2a
Sarvesh Joshi <42295340+sarveshggn@users.noreply.github.com>
2020-05-02 22:46:54 +0530

    Updated send_sms.py for updated version of twilio
    Changed TwilioRestClient To Client as it has been changed with the newer version of twilio. As with TwilioRestClient it was throwing back this error ---- "twilio.base.obsolete.ObsoleteException: TwilioRestClient has been removed from this version of the library."

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,8 @@
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 from credentials import account_sid, auth_token, my_cell, my_twilio
 
 # Find these values at https://twilio.com/user/account
-client = TwilioRestClient(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
 my_msg = "Your message goes here..."
 
